d48e9a5097f6ff80f71c510e3d2568f220dbb852
Limchasu <mayblue20@naver.com>
2024-06-12 23:14:24 +0900

    first commit

---
+++ src/main/java/com/busanit501/teamproject2/lcs/controller/lcsController.java
@@ -0,0 +1,4 @@
+package com.busanit501.teamproject2.lcs.controller;
+
+public class lcsController {
+}
---
+++ src/main/java/com/busanit501/teamproject2/ohj/controller/ohjController.java
@@ -0,0 +1,4 @@
+package com.busanit501.teamproject2.ohj.controller;
+
+public class ohjController {
+}

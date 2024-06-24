01b254ccf21039cec2a2a8f213abe1f567af4f59
Limchasu <mayblue20@naver.com>
2024-06-24 21:08:53 +0900

    msy 파일 merge시 오류가 발생하여 직접 확인 후 재merge 진행

--- src/main/java/com/busanit501/teamproject2/msy/controller/msyController.java
+++ src/main/java/com/busanit501/teamproject2/msy/controller/msyController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class msyController {
 
-    @GetMapping("/msy/list")
-    public String showIndexPage() {
-        return "msy/list";
-    }
+    @GetMapping("/msy/join")
+    public String join() {return "msy/join";}
+
+    @GetMapping("/msy/login")
+    public String login() {return "msy/login";}
 }
